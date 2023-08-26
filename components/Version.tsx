aa9db9a55ee21920a866e89bc384f1f3f1178762
Ray <i@mk1.io>
2023-08-26 14:56:52 +0800

    style: revert

--- components/Version.tsx
+++ components/Version.tsx
@@ -1,8 +1,14 @@
 import styles from "../styles/version.module.css";
 
-export default () => (
-	<div className={styles.box}>
-		<div className={styles.left}>Retalk</div>
-		<div className={styles.right}>v0.1.0</div>
-	</div>
-);
+export default function Version() {
+    return (
+        <div className={styles.box}>
+            <div className={styles.left}>
+                Retalk
+            </div>
+            <div className={styles.right}>
+                v0.1.0
+            </div>
+        </div>
+    )
+}
